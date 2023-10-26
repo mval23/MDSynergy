e999767de88bedc7a6928344abaa984fda98f719
mval23 <111825282+mval23@users.noreply.github.com>
2023-10-26 18:44:29 -0500

    I tried
    Co-Authored-By: leodavid0109 <137718365+leodavid0109@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from chatgpt import *
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
-
 from streamlit_option_menu import option_menu
+
+from chatgpt import *
 
 selected = "Home"
 history = []
@@ -11,23 +10,35 @@ history = []
 # 1. as sidebar menu
 with st.sidebar:
     # Define a custom style for the option menu to make the background transparent
-    custom_style = {
-        "container": {"background-color": "transparent"},
-        "nav-link-selected": {"background-color": "transparent"},
-    }
+    custom_style = {"container": {"background-color": "transparent"},
+                    "nav-link-selected": {"background-color": "transparent"}, }
 
     selected = option_menu("Menu", ["Home", 'MD Stockbot', "Prediction", "Tests"],
-                           icons=['house', 'chat-left-dots-fill', 'bar-chart-line', 'bug'],
-                           menu_icon="-dots",
+                           icons=['house', 'chat-left-dots-fill', 'bar-chart-line', 'bug'], menu_icon="-dots",
                            default_index=1, styles=custom_style)
 
 if selected == 'Home':
     # Display the content for MD Synergy
-    st.title("MD Synergy")
-    st.write("Welcome to the MD Synergy page.")
-    # todo: add content - David
+    st.markdown("""
+            <div style='display: flex; justify-content: center; align-items: center; height: 8vh;'>
+                <h1>MD Synergy</h1>
+            </div>
+            """, unsafe_allow_html=True)
+    st.markdown("""
+            <div style='display: flex; justify-content: center; align-items: center; height: 2vh;'>
+                <br>
+            </div>
+            """, unsafe_allow_html=True)
+
+    st.markdown("""
+            <div style='display: flex; justify-content: center; align-items: center; height: 5vh;'>
+                <h3>by MDSynergy</h3>
+            </div>
+            """, unsafe_allow_html=True)
+    st.write("Welcome to the MD Synergy page.")  # todo: add content - David
 
 elif selected == 'MD Stockbot':
+    current_message = {'input': None, 'content': None, 'img': None}
     if 'messages' not in st.session_state:
         st.session_state['messages'] = []
 
@@ -51,6 +62,7 @@ elif selected == 'MD Stockbot':
     # st.markdown('by MDSynergy')
 
     user_input = st.text_input('Your input:')
+    current_message['input'] = user_input
 
     st.markdown("""
         <div style='display: flex; justify-content: center; align-items: center; height: 2vh;'>
@@ -80,18 +92,104 @@ elif selected == 'MD Stockbot':
 
                 if function_name == 'plot_stock_price':
                     st.image('stock.png')
+                    image = Image.open('stock.png')  # Replace 'your_image.png' with your image file path
+                    image_array = np.array(image)
+                    current_message['img'] = image_array
                 else:
                     st.session_state['messages'].append(response_message)
                     st.session_state['messages'].append(
                         {'role': 'function', 'name': function_name, 'content': function_response})
                     second_response = openai.ChatCompletion.create(model='gpt-3.5-turbo-0613',
                                                                    messages=st.session_state['messages'])
-                    st.text(second_response['choices'][0]['message']['content'])
+                    message = second_response['choices'][0]['message']['content']
+                    st.markdown("""
+                                            <style>
+                                            .paragraph-section {
+                                                padding: 20px;
+                                                border-radius: 5px;
+                                            }
+                                            </style>
+                                            """, unsafe_allow_html=True, )
+                    st.markdown(f"""
+                                                <div class="paragraph-section">
+                                                    {message}
+                                                </div>
+                                                """, unsafe_allow_html=True, )
                     st.session_state['messages'].append(
                         {'role': 'assistant', 'content': second_response['choices'][0]['message']['content']})
-            else:
-                st.text(response_message['content'])
-                st.session_state['messages'].append({'role': 'assistant', 'content': response_message['content']})
+                    current_message['content'] = message
 
+            else:
+                message = response_message['content']
+                st.markdown("""
+                                                        <style>
+                                                        .paragraph-section {
+                                                            padding: 20px;
+                                                            border-radius: 5px;
+                                                        }
+                                                        </style>
+                                                        """, unsafe_allow_html=True, )
+                st.markdown(f"""
+                                                            <div class="paragraph-section">
+                                                                {message}
+                                                            </div>
+                                                            """, unsafe_allow_html=True, )
+                current_message['content'] = message
+                st.session_state['messages'].append({'role': 'assistant', 'content': response_message['content']})
         except Exception as e:
             raise e
+    history.append(current_message)
+    st.write(history)
+
+elif selected == 'Prediction':
+    # Display the content for the Prediction option
+    st.title("Prediction")
+    st.write("Welcome to the Prediction page.")
+
+elif selected == 'Tests':
+    st.markdown("""
+        <div style='display: flex; justify-content: center; align-items: center; height: 8vh;'>
+            <h1>Debuggg :p</h1>
+        </div>
+        """, unsafe_allow_html=True)
+    st.markdown("""
+        <div style='display: flex; justify-content: center; align-items: center; height: 2vh;'>
+            <br>
+        </div>
+        """, unsafe_allow_html=True)
+
+    st.markdown("""
+        <div style='display: flex; justify-content: center; align-items: center; height: 5vh;'>
+            <h3>by MDSynergy</h3>
+        </div>
+        """, unsafe_allow_html=True)
+
+    # Define the CSS class
+    st.markdown("""
+    <style>
+    .paragraph-section {
+        padding: 20px;
+        border-radius: 5px;
+        background-color: #f0f0f0;  /* You can customize the background color */
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    st.write(history)
+    # Display messages and images
+    for message in history:
+        st.markdown(f"""
+            <div class="paragraph-section">
+                {message['input']}
+            </div>
+            """, unsafe_allow_html=True)
+
+        if message['img'] is not None:
+            st.image(message['img'], caption='Stockbot Plot', use_column_width=True)
+
+        if message['content'] is not None:
+            text_content = message['content']
+            st.markdown(f"""
+                <div class="paragraph-section">
+                    {text_content}
+                </div>
+                """, unsafe_allow_html=True)
