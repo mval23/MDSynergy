df77b332dc79ee309423321fedebbd6d426abf16
leodavid0109 <leodavid0521@gmail.com>
2023-10-26 22:14:09 -0500

    Search history added
    Need to add the "Interface".
    Verify the two options when it changes page
    
    Co-Authored-By: mval23 <111825282+mval23@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from streamlit_option_menu import option_menu
 from chatgpt import *
 
 selected = "Home"
-history = []
+
+# Creación del título
+st.set_page_config(page_title="MDSynergy")
+
+# if 'messages' not in st.session_state:
+#     history = st.session_state['messages']
 
 # 1. as sidebar menu
 with st.sidebar:
@@ -13,8 +18,8 @@ with st.sidebar:
     custom_style = {"container": {"background-color": "transparent"},
                     "nav-link-selected": {"background-color": "transparent"}, }
 
-    selected = option_menu("Menu", ["Home", 'MD Stockbot', "Prediction", "Tests"],
-                           icons=['house', 'chat-left-dots-fill', 'bar-chart-line', 'bug'], menu_icon="-dots",
+    selected = option_menu("Menu", ["Home", 'MD Stockbot', "Prediction", "Historic", "Tests"],
+                           icons=['house', 'chat-left-dots-fill', 'bar-chart-line', 'clock', 'bug'], menu_icon="-dots",
                            default_index=1, styles=custom_style)
 
 if selected == 'Home':
@@ -35,12 +40,14 @@ if selected == 'Home':
                 <h3>by MDSynergy</h3>
             </div>
             """, unsafe_allow_html=True)
-    st.write("Welcome to the MD Synergy page.")  # todo: add content - David
+    st.write("Welcome to the MD Synergy page.")  # TODO: add content - David
 
 elif selected == 'MD Stockbot':
     current_message = {'input': None, 'content': None, 'img': None}
     if 'messages' not in st.session_state:
         st.session_state['messages'] = []
+        st.session_state.session_history = []
+        st.session_state.session_history_2 = []
 
     st.markdown("""
         <div style='display: flex; justify-content: center; align-items: center; height: 8vh;'>
@@ -136,15 +143,115 @@ elif selected == 'MD Stockbot':
                                                             """, unsafe_allow_html=True, )
                 current_message['content'] = message
                 st.session_state['messages'].append({'role': 'assistant', 'content': response_message['content']})
+            st.session_state.session_history.append(current_message)
+            st.session_state.session_history_2.insert(0, current_message)
+
+            # Option 2 Print at the same page
+
+            # Define the CSS class
+            st.markdown("""
+                    <style>
+                    .paragraph-section {
+                        padding: 20px;
+                        border-radius: 5px;
+                    }
+                    </style>
+                    """, unsafe_allow_html=True)
+
+            # Display messages and images
+            for message in st.session_state.session_history_2:
+
+                # Verify if the message is the same as the current one
+                if message['input'] == user_input:
+                    continue
+
+                st.markdown(f"""
+                            <div class="paragraph-section">
+                                <b><i>User:</i></b>
+                                {message['input']}
+                            </div>
+                            """, unsafe_allow_html=True)
+
+                if message['img'] is not None:
+                    st.markdown(f"""
+                                            <div class="paragraph-section">
+                                                <b><i>Stockbot:</i></b>
+                                            </div>
+                                            """, unsafe_allow_html=True)
+                    st.image(message['img'], caption='Stockbot Plot', use_column_width=True)
+
+                if message['content'] is not None:
+                    text_content = message['content']
+                    st.markdown(f"""
+                                <div class="paragraph-section">
+                                    <b><i>Stockbot:</i></b>
+                                    {text_content}
+                                </div>
+                                """, unsafe_allow_html=True)
+
         except Exception as e:
             raise e
-    history.append(current_message)
-    st.write(history)
+
 
 elif selected == 'Prediction':
     # Display the content for the Prediction option
     st.title("Prediction")
     st.write("Welcome to the Prediction page.")
+
+elif selected == 'Historic':
+    # Option 1 Historic session researches
+    st.markdown("""
+            <div style='display: flex; justify-content: center; align-items: center; height: 8vh;'>
+                <h1>Search History</h1>
+            </div>
+            """, unsafe_allow_html=True)
+    st.markdown("""
+            <div style='display: flex; justify-content: center; align-items: center; height: 2vh;'>
+                <br>
+            </div>
+            """, unsafe_allow_html=True)
+
+    st.markdown("""
+            <div style='display: flex; justify-content: center; align-items: center; height: 5vh;'>
+                <h3>by MDSynergy</h3>
+            </div>
+            """, unsafe_allow_html=True)
+
+    # Define the CSS class
+    st.markdown("""
+        <style>
+        .paragraph-section {
+            padding: 20px;
+            border-radius: 5px;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+    # Display messages and images
+    for message in st.session_state.session_history:
+        st.markdown(f"""
+                <div class="paragraph-section">
+                    <b><i>User:</i></b>
+                    {message['input']}
+                </div>
+                """, unsafe_allow_html=True)
+
+        if message['img'] is not None:
+            st.markdown(f"""
+                                <div class="paragraph-section">
+                                    <b><i>Stockbot:</i></b>
+                                </div>
+                                """, unsafe_allow_html=True)
+            st.image(message['img'], caption='Stockbot Plot', use_column_width=True)
+
+        if message['content'] is not None:
+            text_content = message['content']
+            st.markdown(f"""
+                    <div class="paragraph-section">
+                        <b><i>Stockbot:</i></b>
+                        {text_content}
+                    </div>
+                    """, unsafe_allow_html=True)
 
 elif selected == 'Tests':
     st.markdown("""
@@ -170,26 +277,6 @@ elif selected == 'Tests':
     .paragraph-section {
         padding: 20px;
         border-radius: 5px;
-        background-color: #f0f0f0;  /* You can customize the background color */
     }
     </style>
     """, unsafe_allow_html=True)
-    st.write(history)
-    # Display messages and images
-    for message in history:
-        st.markdown(f"""
-            <div class="paragraph-section">
-                {message['input']}
-            </div>
-            """, unsafe_allow_html=True)
-
-        if message['img'] is not None:
-            st.image(message['img'], caption='Stockbot Plot', use_column_width=True)
-
-        if message['content'] is not None:
-            text_content = message['content']
-            st.markdown(f"""
-                <div class="paragraph-section">
-                    {text_content}
-                </div>
-                """, unsafe_allow_html=True)
